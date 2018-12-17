86a8374a16a38cf98b00294d29143b5b97fa3637
NotoriousCottonball <gpianka1@gmail.com>
2018-12-17 01:44:01 -0800

    added 'config/environment.rb'

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,10 @@
-ENV['SINATRA_ENV'] ||= "development"
+ENV['RACK_ENV'] ||= "production"
 
 require 'bundler/setup'
-Bundler.require(:default, ENV['SINATRA_ENV'])
+Bundler.require(:default, ENV['RACK_ENV'].to_sym)
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+configure ENV['RACK_ENV'].to_sym do
+  set :database, "sqlite3:db/#{ENV['RACK_ENV']}.sqlite"
+end
 
 require_all 'app'
