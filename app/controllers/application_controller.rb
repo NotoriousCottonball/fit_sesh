93b229041db1b632ebd85a7e25fab0a3b7e8e449
NotoriousCottonball <gpianka1@gmail.com>
2019-01-05 00:12:10 -0800

    Configured ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 require './config/environment'
-
 class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "password_security"
-    register Sinatra::Flash
+    set :session_secret, ENV.fetch('SESSION_SECRET'){`bundle config session_secret`.slice(142..-1)}
   end
+end
