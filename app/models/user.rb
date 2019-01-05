d2a75c4f7a9219589e64c03b9a2768014545d7eb
NotoriousCottonball <gpianka1@gmail.com>
2019-01-05 05:24:20 -0800

    User model and users table migration

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+require_relative "./concerns/slugifiable.rb"
+
+class User < ActiveRecord::Base
+  has_secure_password
+  has_many :workouts
+  has_many :exercises, :through => :workouts
+
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :password, presence: true
+  validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+
+  extend Slugifiable::ClassMethods
+  include Slugifiable::InstanceMethods
+end
---
+++ db/migrate/20190105124758_create_exercise_instances.rb
@@ -0,0 +1,4 @@
+class CreateExerciseInstances < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
